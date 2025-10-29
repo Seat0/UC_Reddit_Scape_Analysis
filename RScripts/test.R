25e795ff90c1fd01fb922839d9390811fe305f92
Seat0 <seansaito924@gmail.com>
2025-10-29 00:05:56 -0700

    updated reddit scrape data

--- RScripts/test.R
+++ RScripts/test.R
@@ -2,4 +2,11 @@ library(tidyverse)
 library(jsonlite)
 library(readr)
 
-test <- fromJSON("Python_code/ucr_posts.JSON")
+test <- fromJSON("../data/UC_data//posts_v2/uci_posts.JSON")
+
+test2 <- n_distinct(test$title)
+
+duplicates <- test %>%
+  add_count(title) %>%
+  filter(n > 1) %>%
+  select(-n)
